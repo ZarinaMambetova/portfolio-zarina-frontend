794ce2450ab8365bae8d2b35c1a8ae3edd7e56f1
Mambetova Zarina <zarina.mambetova84@gmail.com>
2025-11-08 01:02:41 +0300

    Страница Моя карьера

--- app/components/career/wayItem.vue
+++ app/components/career/wayItem.vue
@@ -11,7 +11,7 @@ defineProps({
   <div  class="item__content">
     <div class="item__row item__row-top">
       <div class="item__column">
-        <img :src="item.logo" :alt="логотоп" class="item__image" />
+        <img :src="item.logo" alt="логотип" class="item__image" />
       </div>
 
       <div class="item__column">
@@ -27,8 +27,7 @@ defineProps({
         <li class="item_charge__item"
         v-for="charge in item.charges" 
         :key="charge.id">
-      <img :src="item.logo" :alt="логотоп" class="item__image" />
-      <p  class="item__desc">{{ item.desc }}</p>
+      {{ charge }}
       </li>
       </ul>
 
@@ -44,6 +43,50 @@ defineProps({
 @use '@/assets/css/variables' as v;
 
 .item {
+  cursor: pointer;
+  margin-bottom: 20px;
+  position: relative;
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: -0.6rem;
+    width: 1rem;
+    height: 1rem;
+    background: v.$main-green;
+    border-radius: 50%;
+  }
+
+  &__content {
+    position: relative;
+    padding: 1.5rem;
+    border: .2rem solid v.$bg-color-light;
+    box-shadow: v.$bg-color 0px 0px 15px;
+    background-color: v.$bg-color-light;
+    padding: 1.5rem;
+    border-radius: 10px;
+    border-radius: .6rem;
+    margin: 0 0 2rem 2rem;
+    overflow: hidden;
+
+    &:before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 0;
+      height: 100%;
+      background: v.$bg-color-light;
+      z-index: -1;
+      transition: .5s;
+    }
+
+    &:hover::before {
+        width: 100%;
+    }
+  }
+
   &__image {
     width: 25px;
   }
--- app/components/career/wayList.vue
+++ app/components/career/wayList.vue
@@ -6,50 +6,81 @@ import wayItem from '@/components/career/wayItem.vue';
 const education = ref ([{
   id: 1,
   logo: '/svg/vue.svg',
-  name: 'Астраханский государственный университет',
-  post: 'Факультет мировой экономики и управления',
-  dates:'Март 2003 — Май 2007',
-  desc:'Менеджер',
+  name: 'Loftschool',
+  post: 'UX/UI-дизайн: базовый',
+  dates:'Ноябрь 2019 — Декабрь 2019',
+  charges: ['UI/UX дизайн',  'Sketch',  'Проектирование интерфейсов',  'Figma Design',  'Прототипирование']
 },
 {
   id: 2,
   logo: '/svg/vue.svg',
   name: 'Loftschool',
-  post: 'Веб-разработчик',
-  dates:'Сентябрь 2017 — Январь 2018',
-  charges: [{
-    logo: '/svg/vue.svg',
-    text: '',
-  }]
+  post: 'Комплексное обучение: Javascript',
+  dates:'Октябрь 2018 — Декабрь 2018',
+  desc:'Менеджер',
 },
 {
   id: 3,
   logo: '/svg/vue.svg',
-  name: 'Астраханский государственный университет',
-  post: 'Факультет мировой экономики и управления',
-  dates:'Март 2003 — Май 2007',
-  desc:'Менеджер',
-},
+  name: 'Loftschool',
+  post: 'Веб-разработчик',
+  dates:'Сентябрь 2017 — Январь 2018',
+  charges: [
+     'Vue.js', 'JavaScript',  'Webpack', 'HTML', 'CSS', 'Адаптивная верстка',  'Кроссбраузерная верстка',  'Sass',  'БЭМ',  'Верстка лендингов',  'SVG',  'Семантическая верстка',  'Валидная верстка', 'Figma'
+]},
 {
   id: 4,
   logo: '/svg/vue.svg',
   name: 'Астраханский государственный университет',
   post: 'Факультет мировой экономики и управления',
   dates:'Март 2003 — Май 2007',
-  desc:'Менеджер',
-},])
+  desc:'Менеджер. Государственное и муниципальное управление',
+},
+])
 
 const works = ref ([{
   id: 1,
-  path: '/svg/vue.svg',
-  name: 'VUEJS'
-},])
+  logo: '/svg/vue.svg',
+  name: 'Ctrl2GO',
+  post: 'Фронтенд разработчик (Middle)',
+  dates:'Март 2022 — Апрель 2025 (3 года и 2 месяца)',
+  charges: [
+     'Vue.js', 'Pinia', 'Vite', 'JavaScript', 'Storybook', 'SCSS', 'БЭМ', 'Vue router', 'Git', 'GitLab', 'Axios', 'RESTful API', 'API Яндекс.Карты', 'Swagger', 'SonarQube', 'i18n', 'Figma', 'AmChartsV4 '
+]},
+{
+  id: 2,
+  logo: '/svg/vue.svg',
+  name: 'Транскапиталбанк',
+  post: 'Frontend/HTML-верстальщик (Junior), контент-менеджмент',
+  dates:'Октябрь 2020 — Февраль 2022 (1 год и 5 месяцев)',
+  charges: [
+     'HTML', 'CSS', 'JQuery', 'Адаптивная верстка', 'БЭМ', 'Верстка лендингов', 'Верстка Email', 'Битрикс24', 'Flex', 'PHP'
+]},
+{
+  id: 3,
+  logo: '/svg/vue.svg',
+  name: 'Sliza',
+  post: 'Junior frontend/HTML-верстальщик',
+  dates:'Октябрь 2019 — Июнь 2020 (9 месяцев)',
+  charges: [
+     'HTML', 'CSS', 'Адаптивная верстка', 'Sass', 'БЭМ', 'Верстка лендингов', 'Кроссбраузерная верстка', 'Семантическая верстка', 'JavaScript'
+]},
+{
+  id: 4,
+  logo: '/svg/vue.svg',
+  name: 'Sliza',
+  post: 'Куратор, верстка',
+  dates:'Февраль 2018 — Сентябрь 2019 (1 год и 8 месяцев)',
+  charges: [
+     'Управление людьми', 'Ведение переговоров', 'Построение команды', 'Разработка интерфейсов'
+]},
+])
 </script>
 
 <template>
   <div class="career__content" >
 
-    <titleShared title="Образование"/>
+    <titleShared title="Образование" class="career__title"/>
 
     <transition-group 
       name="slide" 
@@ -64,7 +95,7 @@ const works = ref ([{
 
     </transition-group>
 
-    <titleShared title="Опыт работы"/>
+    <titleShared title="Опыт работы" class="career__title"/>
 
     <transition-group 
       name="slide" 
@@ -85,17 +116,14 @@ const works = ref ([{
 <style lang="scss" scoped>
 @use '@/assets/css/variables' as v;
 
-.main-title {
-  margin-bottom: 40px;
-  font-size: 2.5rem;
-  text-align: center;
-  
-  &__white {
-    color: v.$text-color;
-    margin-right: 15px;
+.career {
+  &__title {
+    margin-bottom: 40px;
   }
-  &__green {
-    color: v.$main-green;
+
+  &__list {
+    position: relative;
+    border-left: .2rem solid v.$main-green;
   }
 }
 

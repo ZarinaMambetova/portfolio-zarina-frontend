ef95beb25bd19c97fda03e7afc9c6dacfa12721d
Mambetova Zarina <zarina.mambetova84@gmail.com>
2025-11-15 22:54:38 +0300

    Работаю над actions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  nitro: {
+    output: {
+      publicDir: 'dist'
+    }
+  },
+
   css: ['~/assets/css/main.scss'],
   
   modules: ['@pinia/nuxt'],

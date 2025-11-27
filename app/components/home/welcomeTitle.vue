616f836afdd423a6ed80626c841b17ee536e6be3
Mambetova Zarina <zarina.mambetova84@gmail.com>
2025-11-28 02:02:35 +0300

    ссылка на резюме

--- app/components/home/welcomeTitle.vue
+++ app/components/home/welcomeTitle.vue
@@ -16,7 +16,7 @@ import IconSvg from '~/assets/icons/download-pdf.svg';
     
         <div class="welcome__buttons">
             <NuxtLink to="#projects" class="welcome__button button__blue button">Проекты</NuxtLink>
-            <a href="@/mambetova-front-resume.pdf" download class="button welcome__button button__black">
+            <a href="./mambetova-front-resume.pdf" download class="button welcome__button button__black">
 
               <span>Резюме</span>
               <IconSvg  class="welcome__icon" />
